2337d2f00b67717cedb8896b9284d04c2327ff69
whatescastelo <wtcastelo@hotmail.com>
2022-03-31 17:07:33 -0300

    Adicionar arquivos

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Revisao{
+    class Program{
+        static void Main(string[] args){
+            Console.WriteLine("DIGITE A OPÇÃO DESEJADA:");
+            Console.WriteLine("1 - Inserir novo aluno.");
+            Console.WriteLine("2 - Listar alunos.");
+            Console.WriteLine("3 - Calcular média geral.");
+        }
+    }
+}
